5d32ec49ef438d8fec16aefcd9fcb3bf43484eea
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-17 23:34:03 +0530

    [Piyush : added project file of generic covered in class]

---
+++ GenericsDemo/PrintArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace GenericsDemo
+{
+	public class PrintArray
+	{
+        public static void toPrint<T>(T[] inputArray)
+        {
+            foreach (T element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------------------");
+        }
+
+        public static void toPrint(int[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------------------");
+        }
+        public static void toPrint(double[] inputArray)
+        {
+            foreach (var element in inputArray)// 1,5,3,8,9
+            {
+                Console.WriteLine(element);
+            }
+           Console.WriteLine("----------------------------------");
+        }
+
+        public static void toPrint(char[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-------------------------------------");
+        }
+
+    }
+}
+
---
+++ GenericsDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using GenericsDemo;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] intArray = { 1, 2, 3, 4, 5 };
+        double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+        char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+        bool[] boolArr = { true, false };
+        PrintArray.toPrint(intArray);
+        PrintArray.toPrint(doubleArray);
+        PrintArray.toPrint(charArray);
+
+        //GenericsMethod.toPrint<int>(intArray);
+        //GenericsMethod.toPrint<double>(doubleArray);
+        //GenericsMethod.toPrint<char>(charArray);
+        //GenericsMethod.toPrint<bool>(boolArr);
+        //PrintArray<int> obj = new PrintArray<int>(intArray);
+        //obj.toPrint();
+
+
+    }
+}
